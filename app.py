bf7d034f98e172affcaedefae789dfdf3fdcc2be
Pooja Ellanti <pe4111@srmist.edu.in>
2026-04-15 20:02:19 +0530

    Modify Flask app to include product listing
    Updated the home route to render a template with product data.

--- app.py
+++ app.py
@@ -1,10 +1,17 @@
-from flask import Flask # type: ignore
+from flask import Flask, render_template
+
 app = Flask(__name__)
+
+products = [
+    {"name": "Laptop", "price": "₹50,000"},
+    {"name": "Phone", "price": "₹20,000"},
+    {"name": "Headphones", "price": "₹2,000"},
+    {"name": "Shoes", "price": "₹3,000"}
+]
 
 @app.route('/')
 def home():
-    return "E-commerce App Running Successfully!"
-
+    return render_template("index.html", products=products)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
